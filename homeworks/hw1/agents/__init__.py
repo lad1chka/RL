36937a5b1fdf43a49a69a72477f4a2c90b965d4e
lad1chka <vlad5476357@gmail.com>
2025-12-30 02:00:11 +0300

    i forget it

---
+++ homeworks/hw1/agents/__init__.py
@@ -0,0 +1,4 @@
+"""This module contains example of agents that can be used to interact with the environment."""
+
+from sumo_rl.agents.ql_agent import QLAgent
+from sumo_rl.agents.dqn_agent import DQNAgent
